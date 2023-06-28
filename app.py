cd0235bb4aeec8017c725092b832d0d1cd9f4026
Kashish Patel <patelkashishpatel032@gmail.com>
2023-06-28 11:58:25 +0530

    added the search functionality

--- app.py
+++ app.py
@@ -8,47 +8,53 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class ToDo(db.Model):
-    sno = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(1000), nullable = False)   
-    desc = db.Column(db.String(10000), nullable = False)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(1000), nullable=False)
+    desc = db.Column(db.String(10000), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-with app.app_context():
-    db.create_all()
-
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
         title = request.form['title']
         desc = request.form['desc']
-        todo = ToDo(title = title, desc = desc)
+        todo = ToDo(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
 
-    Todos = ToDo.query.all()
-    return render_template('index.html', Todos = Todos)
+    query = request.args.get('q')
+    print("Received query:", query)
+    if query:
+        Todos = ToDo.query.filter(ToDo.title.contains(query) | ToDo.desc.contains(query)).all()
+        print("Search results:", Todos)
+    else:
+        Todos = ToDo.query.all()
 
-@app.route('/update/<int:sno>', methods = ['GET','POST'])
+    return render_template('index.html', Todos=Todos, query=query)
+
+
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
     if request.method == 'POST':
         title = request.form['title']
         desc = request.form['desc']
-        todo = ToDo.query.filter_by(sno = sno).first()
+        todo = ToDo.query.filter_by(sno=sno).first()
         todo.title = title
         todo.desc = desc
         db.session.add(todo)
         db.session.commit()
         return redirect("/")
-    todo = ToDo.query.filter_by(sno = sno).first()
+    todo = ToDo.query.filter_by(sno=sno).first()
 
-    return render_template('update.html', todo = todo)
+    return render_template('update.html', todo=todo)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
-    todo = ToDo.query.filter_by(sno = sno).first()
+    todo = ToDo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect("/")
